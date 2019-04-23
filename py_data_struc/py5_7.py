c1ea809b1e8896b840ccdcc0b40af7899c26d238
QI-Xiao <524290558@qq.com>
2019-04-23 20:46:38 +0800

    6

---
+++ py_data_struc/py5_7.py
@@ -0,0 +1,83 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+bubbleSort(alist)
+print(alist)
+
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                exchanges = True
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+        passnum -= 1
+
+alist=[20,30,40,90,50,60,70,80,100,110]
+shortBubbleSort(alist)
+print(alist)
+
+
+def selectionSort(alist):
+    for fillslot in range(len(alist)-1, 0, -1):
+        positionofmax = 0
+        for location in range(1, fillslot+1):
+            if alist[location] > alist[positionofmax]:
+                positionofmax = location
+
+        alist[fillslot], alist[positionofmax] = alist[positionofmax], alist[fillslot]
+
+alist = [54,26,93,17,77,31,44,55,20]
+selectionSort(alist)
+print(alist)
+
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+
+        currentvalue = alist[index]
+        position = index
+
+        while position>0 and alist[position-1]>currentvalue:
+            alist[position] = alist[position - 1]
+            position -= 1
+
+        alist[position] = currentvalue
+
+alist = [54,26,93,17,77,31,44,55,20]
+insertionSort(alist)
+print(alist)
+
+
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist, startposition, sublistcount)
+
+        print("After increments of size", sublistcount, "The list is", alist)
+        sublistcount = sublistcount // 2
+
+
+def gapInsertionSort(alist, start, gap):
+    for i in range(start+gap, len(alist), gap):
+        currentvalue = alist[i]
+        position = i
+
+        while position >= gap and alist[position - gap] > currentvalue:
+            alist[position] = alist[position - gap]
+            position = position - gap
+        alist[position] = currentvalue
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shellSort(alist)
+print(alist)
+
---
+++ py_data_struc/py6_6.py
@@ -0,0 +1,21 @@
+from pythonds.basic.stack import Stack
+from pythonds.trees.binaryTree import BinaryTree
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRootVal(int(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in ['+', '-', '*', '/']:
+            pass
+
